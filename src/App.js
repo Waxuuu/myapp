203b3b8d99afbbe23a35bb9cd2742a6216b06eff
Waxuuu <isao.dossantos@gmail.com>
2022-12-07 17:06:08 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home";
+import Pokemon from "./pages/Pokemon";
+import Pokedex from "./pages/Pokedex";
 
-function App() {
+import{
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route
+} from "react-router-dom";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+<div>
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/Pokemon">Pokemon</Link>
+      </li>
+      <li>
+        <Link to="/Pokedex">Pokedex</Link>
+      </li>
+    </ul>
+  </nav>
+
+  <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Pokemon">
+            <Pokemon />
+          </Route>
+          <Route path="/Pokedex">
+            <Pokedex />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+
+
+
